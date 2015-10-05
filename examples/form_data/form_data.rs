c001383028c973e61d733e13c52cf3c14b61f9d7
robin <robin@ansvia.com>
2015-10-05 15:09:54 +0700

    add plugin for parsing form data.

--- examples/form_data/form_data.rs
+++ examples/form_data/form_data.rs
@@ -1,7 +1,9 @@
 #[macro_use] extern crate nickel;
 use nickel::{Nickel, HttpRouter};
 use std::collections::HashMap;
-use std::io::Read;
+//use std::io::Read;
+
+use nickel::BodyFormDataString;
 
 fn main() {
     let mut server = Nickel::new();
@@ -14,14 +16,26 @@ fn main() {
     });
 
     server.post("/confirmation", middleware!{ |req, res|
-        let mut form_data = String::new();
-        req.origin.read_to_string(&mut form_data).unwrap();
-
-        println!("{}", form_data);
-
+        // let mut form_data = String::new();
+        // req.origin.read_to_string(&mut form_data).unwrap();
+        //
+        // println!("{}", form_data);
+        //
         let mut data = HashMap::new();
         data.insert("title", "Confirmation");
-        data.insert("formData", &form_data);
+        // data.insert("formData", &form_data);
+        //
+        //
+        let form_data = req.form_data();
+        let firstname = form_data.get("firstname").unwrap();
+        let lastname = form_data.get("lastname").unwrap();
+        let phone = form_data.get("phone").unwrap();
+        let email = form_data.get("email").unwrap();
+
+        data.insert("firstname", firstname);
+        data.insert("lastname", lastname);
+        data.insert("phone", phone);
+        data.insert("email", email);
 
         return res.render("examples/form_data/views/confirmation.html", &data)
     });
